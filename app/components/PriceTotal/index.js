744a73991f5ac3726270b6122ba5481388a6ecc1
hayes9321 <t.hayes8321@gmail.com>
2017-04-03 19:18:20 -0700

    added priceItem functionality to priceItem sheet

--- app/components/PriceTotal/index.js
+++ app/components/PriceTotal/index.js
@@ -11,196 +11,88 @@ import actions from '../../actions';
 
 class PriceTotal extends Component {
 
-
-//render method that generates a layout for the infomation to be displayed in 
-  //render(){
-
-    // let storeAge = [];
-    // let prop = parseFloat(this.props.itemSelectedState._root.entries["0"][1].payload.itemSelected)
-    // storeAge.push(prop);
-
-    // let total = storeAge.reduce(addition)
-
-    // function addition(a,b) {
-    //   return a + b;
-    // }
-
-    // console.log()
-
-    /*
-    render() {  
-      const generatePriceList = ( props ) => {
-      
-        <View style={} key={}>
-          <View style={}>
-              <View>
-                  <Text>{this.props.title}</Text>
-                  <Text>{this.props.itemSelected}</Text>
-                  <Text>{this.props.clubPrice}</Text>
-              </View>
-          </View>
-        </priceItem>
-/*      
-      return(
-        <View>
-          <ScrollView>
-            <View style={styles.headerContainer}>
-
-              <View>
-                <Text style={styles.headerText}>Price Totals</Text>
-              </View>
-
-            </View>
-
-            <View style={styles.priceContainer}>
-            
-              <View style={styles.item}>
-                <Text style={styles.categoriesText}>Item</Text>
-              </View>
-
-              <View style={styles.price}>
-                <Text style={styles.categoriesText}>Price</Text>
-              </View>
-
-              <View style={styles.price}>
-                <Text style={styles.categoriesText}>GMC</Text>
-              </View>
-
-            </View>
-
-            <View>
-              {generatePriceList}
-            </View>
-              </ScrollView>
-        </View>
-      ) 
-
-    }
-
-
-    //   <View style={styles.background}>
-    //     <ScrollView>
-    //     <View style={styles.headerContainer}>
-
-    //       <View>
-    //         <View style={styles.headerText}>Price Totals</Text>
-    //       </View>
-
-    //     </View>
-
-    //     <View style={styles.priceContainer}>
-        
-    //       <View style={styles.item}>
-    //         <Text style={styles.categoriesText}>Item</Text>
-    //       </View>
-
-    //       <View style={styles.price}>
-    //         <Text style={styles.categoriesText}>Price</Text>
-    //       </View>
-
-    //       <View style={styles.price}>
-    //         <Text style={styles.categoriesText}>GMC</Text>
-    //       </View>
-
-    //     </View>
-
-    //     <View style={styles.priceContainer}>
-        
-    //       <View style={styles.item}>
-    //         <Text style={styles.itemText}>PTU: GF</Text>
-    //       </View>
-
-    //       <View style={styles.price} >
-    //         <Text style={styles.text}>${total}</Text>
-    //       </View>
-
-    //       <View style={styles.price}>
-    //         <Text style={styles.text}>$248.00</Text>
-    //       </View>
-
-    //     </View>
-
-    //     <View style={styles.priceContainer}>
-        
-    //       <View style={styles.item}>
-    //         <Text style={styles.itemText}>Hot Surface Ignitor</Text>
-    //       </View>
-
-    //       <View style={styles.price}>
-    //         <Text style={styles.text}>$168.00</Text>
-    //       </View>
-
-    //       <View style={styles.price}>
-    //         <Text style={styles.text}>$138.00</Text>
-    //       </View>
-
-    //     </View>
-        
-    //     <View style={styles.itemsContainer}>
-
-    //       <View style={styles.item}>
-    //         <Text style={styles.itemText}>Club Savings:</Text>
-    //       </View>
-
-    //       <View style={styles.price}>
-    //         <Text style={styles.gmcPrice}>+59.00</Text>
-    //       </View>
-
-    //     </View>
-        
-
-    //     <View style={styles.totalContainer}>
-
-    //       <View style={styles.item}>
-    //         <Text style={styles.itemText}>Total Before Tax:</Text>
-    //       </View>
-
-    //       <View style={styles.price}>
-    //         <Text style={styles.regularPrice}>$327.00</Text>
-    //       </View>
-
-    //       <View style={styles.price}>
-    //         <Text style={styles.gmcPrice}>$386.00</Text>
-    //       </View>
-
-    //     </View>
-    //   </ScrollView>
-    //   </View>
-    )
-    
-
-
-  
-
-    */
     render(){
-
-     
     
-    let keyId = this.props.itemSelectedState.itemSelected.id;
-    let title;
-    let itemSelected;
-    let clubPrice;
-  
-
-
+    //create price calc function
 
     const generatePriceList = ( i ) => (
-      <View style={styles.background} key={i.id}>
-        <View style={{flex:1}}>
-                <Text> hello</Text>
-                <Text>{this.props.itemSelected}</Text>
-                <Text>{this.props.clubPrice}</Text> 
-        </View>
+      <View style={styles.priceContainer} key={i.id}>
+        
+          <View style={styles.item}>
+            <Text style={styles.itemText}>{i.itemInfo.title}</Text>
+          </View>
+
+          <View style={styles.price} >
+            <Text style={styles.text}>${i.itemSelected}</Text>
+          </View>
+
+          <View style={styles.price}>
+            <Text style={styles.text}>${i.itemInfo.clubPrice}</Text>
+          </View>
       </View>
     );
 
     return(
+      <View style={styles.background}>
         <ScrollView>
+        <View style={styles.headerContainer}>
+
+          <View>
+            <Text style={styles.headerText}>Price Totals</Text>
+          </View>
+
+        </View>
+
+        <View style={styles.priceContainer}>
+        
+          <View style={styles.item}>
+            <Text style={styles.categoriesText}>Item</Text>
+          </View>
+
+          <View style={styles.price}>
+            <Text style={styles.categoriesText}>Price</Text>
+          </View>
+
+          <View style={styles.price}>
+            <Text style={styles.categoriesText}>GMC</Text>
+          </View>
+
+        </View>
+          
           {this.props.itemSelectedState.itemSelected.map(generatePriceList)}
+
+        <View style={styles.itemsContainer}>
+
+          <View style={styles.item}>
+            <Text style={styles.itemText}>Club Savings:</Text>
+          </View>
+
+          <View style={styles.price}>
+            <Text style={styles.gmcPrice}>+59.00</Text>
+          </View>
+
+        </View>
+        
+
+        <View style={styles.totalContainer}>
+
+          <View style={styles.item}>
+            <Text style={styles.itemText}>Total Before Tax:</Text>
+          </View>
+
+          <View style={styles.price}>
+            <Text style={styles.regularPrice}>$327.00</Text>
+          </View>
+
+          <View style={styles.price}>
+            <Text style={styles.gmcPrice}>$386.00</Text>
+          </View>
+
+        </View>
         </ScrollView>
+      </View>
     )
-    }
+  }
 }
 
 function mapStateToProps(state){
